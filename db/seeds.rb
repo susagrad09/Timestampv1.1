263c62c97b55a4e5489daf95243fc8435b00b863
Felix Tsai <felixtsai@yahoo.com>
2013-02-10 15:06:34 -0800

    add awesome_print gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 GradeLevel.find_or_create_by_year("6th Grade")
-GradeLevel.find_or_create_by_year("6th Grade - DI")
 GradeLevel.find_or_create_by_year("7th Grade")
 GradeLevel.find_or_create_by_year("8th Grade")
 
